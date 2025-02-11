f1db397708a9cc979a6f6c2d0a432397da70839e
Souka001 <soukainaiziki2001@gmail.com>
2025-02-11 19:21:29 +0100

    last modifications

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/#" element={<FirstPage />} />
-        <Route path="/#/todo" element={<ToDoPage />} />
+        <Route path="/#/#todo" element={<ToDoPage />} />
       </Routes>
     </Router>
   );
